bc0739d20624ca2980324386c37ce8574b42fb8b
chummar <chummar@gmail.com>
2015-05-23 17:10:21 -0400

    Adding Jar File

--- src/com/gchart/Main.java
+++ src/com/gchart/Main.java
@@ -10,14 +10,55 @@ import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 import org.w3c.dom.Document;
 
 public class Main {
 
     public static void main(String args[]) throws Exception {
-        File stylesheet = new File("src/main/style1.xsl");
-        File xmlSource = new File("src/main/data1.xml");
+
+
+        /*-----------------*/
+        /*-----------------*/
+        Properties prop = new Properties();
+        InputStream input = null;
+
+        try {
+
+            input = new FileInputStream("src/main/config.properties");
+
+            // load a properties file
+            prop.load(input);
+
+            // get the property value and print it out
+            System.out.println(prop.getProperty("xmlfolder"));
+            System.out.println(prop.getProperty("stylefolder"));
+            System.out.println(prop.getProperty("dbpassword"));
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            if (input != null) {
+                try {
+                    input.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+
+        /*--- */
+
+       // File stylesheet = new File("src/main/style2.xsl");
+        // File xmlSource = new File("src/main/data1.xml");
+
+        File stylesheet = new File(prop.getProperty("stylefolder"));
+        File xmlSource = new File(prop.getProperty("xmlfolder"));
 
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         DocumentBuilder builder = factory.newDocumentBuilder();
@@ -27,7 +68,44 @@ public class Main {
         Transformer transformer = TransformerFactory.newInstance()
                 .newTransformer(stylesource);
         Source source = new DOMSource(document);
-        Result outputTarget = new StreamResult(new File("/tmp/x2.csv"));
+        Result outputTarget = new StreamResult(new File(prop.getProperty("outputfile")));
         transformer.transform(source, outputTarget);
     }
+
+
+    private static void loadProperties()
+    {
+
+
+        /*-----------------*/
+        Properties prop = new Properties();
+        InputStream input = null;
+
+        try {
+
+            input = new FileInputStream("src/main/config.properties");
+
+            // load a properties file
+            prop.load(input);
+
+            // get the property value and print it out
+            System.out.println(prop.getProperty("xmlfolder"));
+            System.out.println(prop.getProperty("stylefolder"));
+            System.out.println(prop.getProperty("dbpassword"));
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            if (input != null) {
+                try {
+                    input.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+
+    }
+
 }
